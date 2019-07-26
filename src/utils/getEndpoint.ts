cf3e115832d62521fee21e6318d1421ec04fd336
Ricky Gonzales <gonzalrick@gmail.com>
2019-07-26 23:58:07 +1000

    Added game period string

--- src/utils/getEndpoint.ts
+++
@@ -1,7 +0,0 @@
-const endpoints: { [key: string]: string } = {
-  production: 'http://localhost:3001/graphql',
-};
-
-export function getEndpoint(environment: string) {
-  return endpoints[environment] || 'http://localhost:3001/graphql';
-}
